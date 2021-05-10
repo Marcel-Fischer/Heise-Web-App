a85ec085328dc83d417debc51d06bb2747f0358f
Marcel-Fischer <marcel-fischer95@web.de>
2021-05-10 18:35:30 +0200

    Removed unnecessary semicolons and imports

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const { response } = require('express');
-const app = express();
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const app = express()
+const secret = 'secret'
+const contentType = 'application/json'
 
 app.use(cors())
 app.use(express.json())
-
-const secret = 'secret'
-const contentType = 'application/json'
 
 app.get('/api', (req, res)=>{
     axios.get(`https://hk-test-api.herokuapp.com/albums/`, {
